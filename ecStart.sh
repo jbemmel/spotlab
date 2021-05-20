604c6c9d2f398dab41cf5aa035c825d313b72196
AWS Lab user <spotlab@bembox.attlocal.net>
2021-05-20 17:43:19 -0500

    Renaming

--- ecStart.sh
+++ ecStart.sh
@@ -11,9 +11,8 @@ export HOME=/home/spotlab
 #
 function show_usage() {
  echo "Copyright (C) 2018-2021 Nokia, all rights reserved. Version 1.0 2021-05-16"
- echo "Usage: docker run -it --rm --device /dev/fuse --cap-add SYS_ADMIN -v $HOME:$HOME:Z eccloud/spotlab [ git ]"
- echo "  (the ':Z' is for SELinux relabeling)"
- echo "  tip: You can add '--dns:x.x.x.x' to specify a DNS server for the Ansible host (this container) to use"
+ echo "Usage: docker run -it --rm -v $HOME:$HOME:Z eccloud/spotlab"
+ echo " (-v mounts a volume for home, the ':Z' is for SELinux relabeling)"
  echo ""
 
  # /orig_adventure/install_adventure.sh 172.17.0.1
@@ -48,77 +47,50 @@ EOF
 fi
 
 # Asks for password if SSH key not registered with git
-AWSLAB_ROOT=/awslab
+SPOTLAB_ROOT=/spotlab
 if [ "$1" == "git" ]; then
  git config --global http.proxy ${http_proxy:-http://proxy.lbs.alcatel-lucent.com:8000}
  git config --global http.sslVerify "false"
 
  # mv /simplify /orig_simplify # Fails
- # rm -rf ${AWSLAB_ROOT}/*
- if [ ! -d /home/awslab/awslab_git ]; then
-  git clone git@gitlab.com:jbemmel/aws-lab.git /home/awslab/awslab_git || true
+ # rm -rf ${SPOTLAB_ROOT}/*
+ if [ ! -d /home/spotlab/spotlab_git ]; then
+  git clone git@gitlab.com:jbemmel/aws-lab.git /home/spotlab/spotlab_git || true
  else
-  cd /home/awslab/awslab_git && git pull
+  cd /home/spotlab/spotlab_git && git pull
  fi
- ln -sf /home/awslab/awslab_git ${AWSLAB_ROOT}
- AWSLAB_RELEASE="[git]"
+ ln -sf /home/spotlab/spotlab_git ${SPOTLAB_ROOT}
+ SPOTLAB_RELEASE="[git]"
 else
- ln -sf /awslab_src ${AWSLAB_ROOT}
+ ln -sf /spotlab_src ${SPOTLAB_ROOT}
 fi
 
 if [ ! -e $HOME/local_settings.yml ]; then
   cat > $HOME/local_settings.yml << EOF
 #
-# Local settings for AWS Lab
+# Local settings for SpotLab
 #
-
-# (optional) server password; while it is bad security practice to keep these in text files,
-# it can be convenient to configure this here rather than enter it on the command line each time
-#
-# become_pass: "xxxx"
-
-#
-# The server password is only used once, to setup SSH password access to each server.
-# It won't be needed after the first run ( per target host )
-#
-# server_password: "XXXX"
-
-#
-# RedHat username/password for subscription manager, used to build Openstack VIM VM
-#
-# redhat_user: "XXXX"
-# redhat_password: "XXXX"
-
-# git username/password for checking out MetroAE
-#
-# git_user: "xxx"
-# git_password: "xxx"
 
 # AWS Credentials and S3 bucket
+#
 # AWS_ACCESS_KEY_ID: "xxxx"
 # AWS_SECRET_ACCESS_KEY: "yyyy"
 # AWS_REGION: "us-east-1"
-# AWSLAB_S3_BUCKET: "awslab-unique-bucketname"
-awslab_cache_downloads_in_s3: true
+# SPOTLAB_S3_BUCKET: "spotlab-unique-bucketname"
+spotlab_cache_downloads_in_s3: true
 
 # Optional HTTP proxy
 # http_proxy: "http://xx"
 # https_proxy: "http://xx"
 # no_proxy: "list of IPs or fqdns"
 
-# NTP servers to use if not specified in designs
-# ntp_servers: []
-
-# DNS servers to use if not specified, read from resolv.conf if not defined here
-# dns_servers: []
-
 EOF
 else
-sed -E 's/:[^:\/\/0-9]/=/g;s/$//g;s/\[//g;s/\]//g;s/ *= */=/g;s/^([^#])/export \1/g' $HOME/local_settings.yml > ${AWSLAB_ROOT}/local_settings.sh
+sed -E 's/:[^:\/\/0-9]/=/g;s/$//g;s/\[//g;s/\]//g;s/ *= */=/g;s/^([^#])/export \1/g' $HOME/local_settings.yml > ${SPOTLAB_ROOT}/local_settings.sh
 fi
 
 # Copy sample config to root dir, if not existing
-# cp -n ${AWSLAB_ROOT}/sample-configs/minimal.json $HOME/nuage.json || true
+# cp -n ${SPOTLAB_ROOT}/sample-configs/minimal.json $HOME/nuage.json || true
 
 # Generate default hosts inventory
 if [ ! -e $HOME/hosts ]; then
@@ -137,10 +109,10 @@ fi
 # Clear Ansible tmp directory
 # [ -d /files/.ansible/tmp ] && rm -rf /files/.ansible/tmp/*
 
-echo "This is a Docker shell for AWS Lab $AWSLAB_RELEASE. Use <CTRL>-(p + q) to exit while keeping the container running - "
-echo "alias 'launch_aws_instance' is defined for your convenience"
-echo "Available host disk space under $HOME: `df -h $HOME | awk '/home\/awslab/ { print $4 }'`"
-cat > /etc/profile.d/awslab.sh << EOF
+echo "This is a Docker shell for SpotLab $SPOTLAB_RELEASE. Use <CTRL>-(p + q) to exit while keeping the container running - "
+echo "alias 'spotlab_launch_aws_instance' is defined for your convenience"
+echo "Available host disk space under $HOME: `df -h $HOME | awk '/home\/spotlab/ { print $4 }'`"
+cat > /etc/profile.d/spotlab.sh << EOF
 # AWS Lab alias entries
 
 # User specific aliases and functions
@@ -149,23 +121,23 @@ alias ssh="setpriv --reuid 1100 \ssh"
 alias scp="setpriv --reuid 1100 \scp"
 fi
 
-alias aled='echo "ls -r VSR*/*" | sftp -i /home/awslab/.ssh/id_rsa sftp@192.11.249.8'
+alias aled='echo "ls -r VSR*/*" | sftp -i /home/spotlab/.ssh/id_rsa sftp@192.11.249.8'
 
-# Run a given playbook as the 'awslab' user, and timestamp the log
+# Run a given playbook as the 'spotlab' user, and timestamp the log
 run_playbook() {
   if [[ "$1" == "git" || "$1" == "root" ]]; then
    SETPRIV="setpriv --reuid 1100 --regid 1100 --clear-groups"
   fi
-  time ANSIBLE_LOG_PATH=$LOG_PATH/ansible-\`date +%F_%R\`.log ANSIBLE_CONFIG=${AWSLAB_ROOT}/ansible.cfg \${SETPRIV} ansible-playbook ${AWSLAB_ROOT}/\$1.yml "\${@:2}"
+  time ANSIBLE_LOG_PATH=$LOG_PATH/ansible-\`date +%F_%R\`.log ANSIBLE_CONFIG=${SPOTLAB_ROOT}/ansible.cfg \${SETPRIV} ansible-playbook ${SPOTLAB_ROOT}/\$1.yml "\${@:2}"
 }
 
 _mount_s3() {
-  echo "Mounting S3 bucket '\$AWSLAB_S3_BUCKET' on \$HOME/s3..."
+  echo "Mounting S3 bucket '\$SPOTLAB_S3_BUCKET' on \$HOME/s3..."
   mkdir -p "\$HOME/s3" "\$HOME/s3-cache"
-  AWSACCESSKEYID=\$AWS_ACCESS_KEY_ID AWSSECRETACCESSKEY=\$AWS_SECRET_ACCESS_KEY s3fs "\$AWSLAB_S3_BUCKET" "\$HOME/s3" -o use_rrs=1 -o use_cache="\$HOME/s3-cache"
+  AWSACCESSKEYID=\$AWS_ACCESS_KEY_ID AWSSECRETACCESSKEY=\$AWS_SECRET_ACCESS_KEY s3fs "\$SPOTLAB_S3_BUCKET" "\$HOME/s3" -o use_rrs=1 -o use_cache="\$HOME/s3-cache"
 }
 
-alias launch_aws_instance="run_playbook launch_instance"
+alias spotlab_launch_aws_instance="run_playbook launch_instance"
 alias mount_s3="_mount_s3"
 
 export LOG_PATH=$LOG_PATH
@@ -176,30 +148,30 @@ export PARAMIKO_HOST_KEY_AUTO_ADD=True
 export TZ="America/Chicago"
 
 # Source local settings
-if [ -f ${AWSLAB_ROOT}/local_settings.sh ]; then
-	. ${AWSLAB_ROOT}/local_settings.sh
+if [ -f ${SPOTLAB_ROOT}/local_settings.sh ]; then
+	. ${SPOTLAB_ROOT}/local_settings.sh
 fi
 
-# Set prompt to include ADVenture release and host
-PS1="[AWS Lab $AWSLAB_RELEASE on ${AWSLAB_HOST:-?} \W]\\$ "
+# Set prompt to include release and host
+PS1="[SpotLab $SPOTLAB_RELEASE on ${SPOTLAB_HOST:-?} \W]\\$ "
 
 EOF
 
-# Change ownership to our awslab user
-chown -R awslab:awslab $HOME
+# Change ownership to our spotlab user
+chown -R spotlab:spotlab $HOME
 
-#su - adventure -c "cd ${AWSLAB_ROOT} && \
+#su - adventure -c "cd ${SPOTLAB_ROOT} && \
 #  NODE_PATH=/usr/local/ansible/node_modules/ ANSIBLE_SKIP_TAGS='${ANSIBLE_SKIP_TAGS}' \
-#  LOG_PATH='${LOG_PATH}' AWSLAB_RELEASE='${AWSLAB_RELEASE}' node ./web-ui/backend/webserver.js" > $LOG_PATH/web.log 2>&1 &
+#  LOG_PATH='${LOG_PATH}' SPOTLAB_RELEASE='${SPOTLAB_RELEASE}' node ./web-ui/backend/webserver.js" > $LOG_PATH/web.log 2>&1 &
 
 # Update web UI and Digitizer to show ADVenture release in footer ( TODO separate single .js file )
-# sed -i "s/\[\[SIMPLIFY_RELEASE\]\]/$SIMPLIFY_RELEASE/g" ${AWSLAB_ROOT}/web-ui/index.html
-# sed -i "s/\[\[SIMPLIFY_RELEASE\]\]/$SIMPLIFY_RELEASE/g" ${AWSLAB_ROOT}/designer/js/Dialogs.js
+# sed -i "s/\[\[SIMPLIFY_RELEASE\]\]/$SIMPLIFY_RELEASE/g" ${SPOTLAB_ROOT}/web-ui/index.html
+# sed -i "s/\[\[SIMPLIFY_RELEASE\]\]/$SIMPLIFY_RELEASE/g" ${SPOTLAB_ROOT}/designer/js/Dialogs.js
 
 if [[ "$1" == "git" || "$1" == "root" ]]; then
 cd $HOME && /bin/bash --rcfile /etc/bashrc
 else
-su awslab --login
+su spotlab --login
 fi
 
 exit $?
